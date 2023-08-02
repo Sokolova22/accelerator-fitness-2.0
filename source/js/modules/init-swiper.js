a05116d3c630166db4fcfe38cd74e3bf88ed8326
Sokolova22 <camelia201103@gmail.com>
2023-08-02 23:39:12 +0500

    вносит правки

--- source/js/modules/init-swiper.js
+++ source/js/modules/init-swiper.js
@@ -1,6 +1,7 @@
+import { Swiper } from './vendor/swiper';
 
 export const swiper = new Swiper('.coaches__swiper', {
-  // Optional parameters
+
   loop: true,
 
   breakpoints: {
@@ -8,21 +9,19 @@ export const swiper = new Swiper('.coaches__swiper', {
       slidesPerView: 1,
       spaceBetween: 30,
     },
+
     768: {
       slidesPerView: 2,
       spaceBetween: 30,
     },
-    // 1200: {
-    //   slidesPerView: 4,
-    //   spaceBetween: 10,
-    // },
+
     1200: {
       slidesPerView: 4,
       spaceBetween: 40,
     },
   },
 
-  // Navigation arrows
+
   navigation: {
     nextEl: '.coaches__swiper-button--next',
     prevEl: '.coaches__swiper-button--prev',
@@ -31,10 +30,10 @@ export const swiper = new Swiper('.coaches__swiper', {
 
 
 export const swiperFeedback = new Swiper('.feedback__swiper', {
-  // Optional parameters
+
   slidesPerView: 1,
 
-  // Navigation arrows
+
   navigation: {
     nextEl: '.feedback__swiper-button--next',
     prevEl: '.feedback__swiper-button--prev',
